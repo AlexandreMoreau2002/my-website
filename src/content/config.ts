523a0dc86b4a19ebc657b65f43add5300576490e
Alexandre <moreaualexandre2002@gmail.com>
2025-04-12 19:40:57 +0200

    fix(lang-toggle): correct language switching and project link

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,18 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z, type CollectionEntry } from 'astro:content'
+
+export type WorkEntry = CollectionEntry<'work'>
 
 export const collections = {
-	work: defineCollection({
-		type: 'content',
-		schema: z.object({
-			title: z.string(),
-			description: z.string(),
-			publishDate: z.coerce.date(),
-			tags: z.array(z.string()),
-			img: z.string(),
-			img_alt: z.string().optional(),
-			lang: z.enum(['fr', 'en'])
-		}),
-	}),
-};
+  work: defineCollection({
+    type: 'content',
+    schema: z.object({
+      title: z.string(),
+      description: z.string(),
+      publishDate: z.coerce.date(),
+      tags: z.array(z.string()),
+      img: z.string(),
+      img_alt: z.string().optional(),
+      lang: z.enum(['fr', 'en']),
+    }),
+  }),
+}
--- src/utils/generateUrl.ts
+++
@@ -1,4 +0,0 @@
-export default function generateUrl(locale: string, path: string): string {
-  path = path.startsWith('/') ? path.slice(1) : path;
-  return `/${locale}/${path}`;
-}
